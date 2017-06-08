1b4a987e85c3667e4cf6e8fa56d0a169b54a6158
rohanrane <rohanr@vt.edu>
2017-06-08 11:01:21 -0400

    Added more functionality

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ var req = require('superagent');
 
 var googleapi = 'AIzaSyA-5tK376e77b7JBJkguMkzFig1Q572iQI';
 
+var distance = 16100;
+
+var currentPlace;
+
 var reprompt = "I didn't hear what you said could you repeat that.";
 skillService.launch(function(request, response) {
   var prompt = 'Welcome to Food Finder';
@@ -27,24 +31,40 @@ skillService.intent('FinderIntent',{
   'slots':[{'RES': 'AMAZON.Language'}],
   'utterances': ['{Where should I go to eat}', '{-|RES}']
 }, function(request, response){
-	console.log('start');
-	console.log(request);
 	var userInput = request.slot('RES');
 	if (userInput == 'yes') {
+		console.log(currentPlace);
+		response.say("I've sent the information to your phone");
+		response.card({
+  			type: "Simple",
+  			title: currentPlace.name,
+  			content: currentPlace.vicinity
+		});
 		response.shouldEndSession(true).send();
 	}
 
+	var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.5407,-77.4360&radius='
+		.concat(distance).concat('&type=restaurant&key=').concat(googleapi);
+
 	return req
-	.post('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.5407,-77.4360&radius=16100&type=restaurant&key=AIzaSyA-5tK376e77b7JBJkguMkzFig1Q572iQI')
+	.post(url)
 	.set('Content-Type', 'application/json')
 	.then(res => {
 		console.log(res.status);
 		console.log(res.body);
 
 		var numLoc = res.body.results.length;
+
+		if (numLoc == 0) {
+			response.say('No results found in area');
+			response.shouldEndSession(false).send();
+			return;
+		}
+
 		var index = Math.floor(Math.random() * numLoc);
 		var name = res.body.results[index].name;
 		var rating = res.body.results[index].rating;
+		currentPlace = res.body.results[index];
 		console.log(name);
 		console.log(rating);
 
@@ -53,6 +73,16 @@ skillService.intent('FinderIntent',{
 			response.shouldEndSession(false).send();
 		}
     });
+  }
+);
+
+skillService.intent('RadiusIntent',{
+  'slots':{'DIST': 'AMAZON.NUMBER'},
+  'utterances': ["{Set distance to } {DIST} {miles}"]
+}, function(request, response){
+	console.log('radius');
+   	distance = 1609*request.slot('DIST');
+   	response.say('Distance set to ' + request.slot('DIST') + ' miles').shouldEndSession(false);
   }
 );
 
